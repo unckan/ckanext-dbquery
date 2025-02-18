61fcfe7ee420dc80f17247f519a97c3f329e9093
german <germankay@gmail.com>
2025-02-18 12:02:22 -0300

    query_database conections

--- ckanext/dbquery/plugin.py
+++ ckanext/dbquery/plugin.py
@@ -1,22 +1,37 @@
+import logging
 import psycopg2
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+log = logging.getLogger(__name__)
+
+
 def query_database():
     """ Realiza una consulta a la base de datos y retorna los resultados """
-    conn = psycopg2.connect(
-        dbname="tu_base",
-        user="tu_usuario",
-        password="tu_password",
-        host="localhost",
-        port="5432"
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM tu_tabla LIMIT 10")
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
+    # Obtén los parámetros de conexión desde la configuración (puedes definirlos en ckan.ini)
+    dbname = toolkit.config.get('dbquery.dbname', 'tu_base')
+    user = toolkit.config.get('dbquery.user', 'tu_usuario')
+    password = toolkit.config.get('dbquery.password', 'tu_password')
+    host = toolkit.config.get('dbquery.host', 'localhost')
+    port = toolkit.config.get('dbquery.port', '5432')
+
+    try:
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM tu_tabla LIMIT 10")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        log.error("Error al realizar la consulta a la base de datos: %s", e)
+        raise
 
     return [{"columna1": row[0], "columna2": row[1]} for row in rows]
 
@@ -32,12 +47,13 @@ class DbqueryPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "dbquery")
 
+    # IActions: Registra la acción custom 'custom_query'
     def get_actions(self):
-        """ Registra la acción custom_query """
         return {
-            "custom_query": self.custom_query
+            "custom_query": DbqueryPlugin.custom_query
         }
 
-    def custom_query(self, context, data_dict):
-        """ Realiza una consulta a la base de datos y retorna los resultados """
+    @staticmethod
+    def custom_query(context, data_dict):
+        """Realiza una consulta a la base de datos y retorna los resultados."""
         return query_database()
