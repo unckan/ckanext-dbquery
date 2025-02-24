55b5fd02cf2b7480e712031aaeb88986d3473440
german <germankay@gmail.com>
2025-02-24 12:05:35 -0300

    templates andando

--- ckanext/dbquery/blueprints/dbquery.py
+++ ckanext/dbquery/blueprints/dbquery.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, render_template, request, flash
 from ckan.plugins import toolkit
+from ckan.lib import base
 
-dbquery_bp = Blueprint('dbquery', __name__, url_prefix='/dbquery')
+dbquery_bp = Blueprint('dbquery', __name__, url_prefix='/ckan-admin/dbquery')
 
 
-@dbquery_bp.route('/index', methods=['GET', 'POST'], endpoint='index')
+@dbquery_bp.route('/index', methods=['GET', 'POST'])
 def index():
     """
     Vista de administración para la extensión DBQuery.
     Aquí puedes, por ejemplo, permitir que el usuario ingrese el nombre
     de una tabla para realizar la consulta.
     """
+    if not toolkit.c.userobj or not toolkit.c.userobj.sysadmin:
+        base.abort(403, ("Need to be system administrator to administer"))
     results = None
     if request.method == 'POST':
         table = request.form.get('table')
