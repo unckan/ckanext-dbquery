d8ec8e1a06b09d3ee9b6d83b3f30d1e84368ee20
german <germankay@gmail.com>
2025-03-25 10:25:43 -0300

    limpieza

--- ckanext/dbquery/tests/conftest.py
+++ ckanext/dbquery/tests/conftest.py
@@ -1,62 +1,27 @@
 import pytest
-import ckan.tests.factories as factories
-from ckanext.dbquery.model import DBQueryExecuted
+from ckan.tests import factories
 
 
 @pytest.fixture
-def clean_db(reset_db_fixture, migrate_db_for):
-    reset_db_fixture()
-    migrate_db_for('dbquery')
-
-
-@pytest.fixture(autouse=True)
-def load_standard_plugins(with_plugins):
-    """ Use 'with_plugins' fixture in ALL tests """
-    pass
-
-
-@pytest.fixture
-def sysadmin_user():
-    """Create a sysadmin user."""
-    user = factories.Sysadmin()
-    return user
+def clean_db(reset_db):
+    reset_db()
 
 
 @pytest.fixture
 def normal_user():
-    """Create a normal user."""
     user = factories.User()
     return user
 
 
 @pytest.fixture
-def mock_executed_queries():
-    """Create some mock executed queries."""
-    user = factories.User()
+def mock_executed_queries(clean_db, sysadmin):
+    from ckanext.dbquery.model import DBQueryExecuted
 
     queries = [
-        DBQueryExecuted(
-            query="SELECT * FROM package",
-            user_id=user['id']
-        ),
-        DBQueryExecuted(
-            query="SELECT * FROM user",
-            user_id=user['id']
-        ),
+        DBQueryExecuted(query="SELECT * FROM package", user_id=sysadmin['id']),
+        DBQueryExecuted(query="SELECT * FROM user", user_id=sysadmin['id']),
     ]
-
-    for query in queries:
-        query.save()
+    for q in queries:
+        q.save()
 
     return queries
-
-
-@pytest.fixture
-def app():
-    """Return a WSGI application for use by tests."""
-    from ckan.config.middleware import make_app
-    from ckan.cli import load_config
-
-    config = load_config('/path/to/test.ini')
-    app = make_app(config)
-    return app
