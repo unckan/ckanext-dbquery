c60b11684e17b621aaeaf3f11b019be397a8cba5
German Favaro <germankay@gmail.com>
2025-06-12 12:07:30 -0300

    Error if bad query

--- ckanext/dbquery/blueprints/dbquery.py
+++ ckanext/dbquery/blueprints/dbquery.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from ckan.plugins import toolkit
+from ckan.plugins.toolkit import ValidationError
 
 
 dbquery_bp = Blueprint('dbquery', __name__, url_prefix='/ckan-admin/db-query')
@@ -10,12 +11,13 @@ def index():
     """
     Run a custom query on the database
     """
-    # Verificar que el usuario sea un administrador del sistema
+    # Check that the user is a system administrator
     if not toolkit.c.userobj or not toolkit.c.userobj.sysadmin:
         return toolkit.abort(403)
 
     query = None
     result = None
+    error_message = None
     request = toolkit.request
 
     # Process form submission
@@ -24,12 +26,17 @@ def index():
         query = form.get('query')
         if query:
             data_dict = {'query': query}
-            result = toolkit.get_action('query_database')(None, data_dict)
+            try:
+                result = toolkit.get_action('query_database')(None, data_dict)
+            except ValidationError as e:
+                # Extract the error message to display to the user
+                error_message = e.error_dict.get('query') if hasattr(e, 'error_dict') else str(e)
 
     # Display results if any
     extra_vars = {
         'result': result,
         'query': query,
+        'error_message': error_message,
     }
 
     return toolkit.render('dbquery/index.html', extra_vars=extra_vars)
